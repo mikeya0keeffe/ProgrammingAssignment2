ba9973aa5366d424e8525aa59e6f382ecbd5bf1f
mikeya0keeffe <mikeya0keeffe@gmail.com>
2018-05-31 16:41:43 +0100

    First and final commit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take an ordinary matrix object, store it
+## and calculate its inverse before also storing this.
 
-## Write a short comment describing this function
+library(matrixcalc)
+
+## The makeCacheMatrix assigns the matrix x and the inverse
+## in its local environment. The get() and getInverse() functions
+## return the respective values whie the set and setInverse()
+## functions assign the first arguments to the respective values 
+## of the parent environment demonstrating lexical scoping.
+## a list is returned containing the 4 functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    get <- function(){
+        x
+    }
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    setInverse <- function(inv){
+        inverse <<- inv
+    }
+    getInverse <- function(){
+        inverse
+    }
+    list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first checks that the matrix x is square and
+## provides a warning if it is not. It then gets the inverse
+## and checks to see if it is null. If the inverse exists then
+## it is returned. Otherwise the inverse is calculated, stored
+## and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if(!is.square.matrix(x$get())){
+        warning("Matrix x is not square.")
+        return()
+    }
+    i <- x$getInverse()
+    if(!is.null(i)){
+        return(i)
+    }
+    i <- solve(x$get())
+    x$setInverse(i)
+    i
 }
